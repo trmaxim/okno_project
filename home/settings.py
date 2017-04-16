46250dc973b3d5c349120d2db862d84aff604721
int_var <int_var@mail.ru>
2017-04-16 12:54:05 +0400

    Modifited settings

--- home/settings.py
+++ home/settings.py
@@ -131,6 +131,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
